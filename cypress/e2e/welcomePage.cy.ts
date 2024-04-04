bc4d1e7dc9b8396d7ea29bdfcbccd2968b39d4ea
Artemy Gibson <artemygibson@gmail.com>
2024-04-04 12:37:33 -0700

    Add tests for dropdown menu to select game theme

--- cypress/e2e/welcomePage.cy.ts
+++ cypress/e2e/welcomePage.cy.ts
@@ -13,9 +13,9 @@ describe('Welcome Page', () => {
   //   cy.get("button").contains("Close").should("be.visible");
   //   cy.get("button").contains("Close").click();
 
-  //   //Which of the below is more preferable? 
-  //   cy.get("h3").contains("Instructions").should("not.be.visible"); // might show a false positive?
-  //   cy.get("h3").contains("Instructions").should("not.exist");
+    //Which of the below is more preferable? 
+    // cy.get("h3").contains("Instructions").should("not.be.visible"); // might show a false positive?
+    // cy.get("h3").contains("Instructions").should("not.exist");
   // });
 
   // it('Does not have instructional pop up if I have visited page before', () => {
@@ -25,15 +25,22 @@ describe('Welcome Page', () => {
   //   cy.get("h3").contains("Instructions").should("not.exist");
   // });
 
-  // it('Has a dropdown menu to pick game themes', () => { // What does () => mean?
-  //   // check that default option is "Pick a theme"
-  // });
+  it('Has a dropdown menu to pick game themes', () => { // What does () => mean?
+    cy.get("select").select("Select a Theme").should("have.value", "random");
+    cy.get("select").select("Animals").should("have.value", "animals");
+    cy.get("select").select("Flowers").should("have.value", "flowers");
+    cy.get("select").select("Fruits").should("have.value", "fruits");
+  });
 
-  // it('Has a button to Play Game which redirects to gameplay page', () => {
+  it('Has a button to Play Game which redirects to gameplay page', () => {
+    cy.get("select").select("Animals");
+    cy.get("input").click;
+    // cy.url().should('include', 'playGame?theme=animals'); // actual page url can be updated later
+  });
 
-  // });
-
-  // it('If game theme is not selected, a randomized theme is chosen', () => {
-
-  // });
+  it('If game theme is not selected, a randomized theme is chosen', () => {
+    cy.get("select").select("Select a Theme");
+    cy.get("input").click;
+    // cy.url().should('include', 'playGame?theme=random'); // actual page url can be updated later
+  });
 });
