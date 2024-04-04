a611cb29ed03a1ed9a12e97b7624d12b5f29c471
Artemy Gibson <artemygibson@gmail.com>
2024-04-04 12:38:46 -0700

    Add dropdown menu, remove popup functionality for now

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -5,17 +5,14 @@ const Welcome = () => {
 
   const [returningUser, setReturningUser] = useState<boolean>(false);
 
-  function hidePopup() {
-    console.log("test");
-    if (returningUser === true) {
-      const popup = document.getElementById('myPopup');
-      if (popup) {
-        popup.style.visibility = "invisible";
-      }
-    }
-  }
+  // function hidePopup() {
+  //   if (returningUser === false) {
+  //     var popup = document.getElementsByClassName('popup');
+  //     popup[0].classList.toggle("show");
+  //   }
+  // }
 
-  hidePopup();
+  // hidePopup();
 
   return (
     <section className="WelcomePage">
@@ -23,18 +20,29 @@ const Welcome = () => {
         <h2>Welcome to Memory Match!</h2>
       </header>
       <div className="popup">
-        <span className="popuptext" id="myPopup">
+        <div className="popuptext">
         <h3>Instructions</h3>
         <p>You will see a series of cards on your screen. They will be facing you for only a few seconds.
         Use those seconds to memorize the cards as best as you can! All cards will have an image on them that matches with another card somewhere else on your screen.
         The cards will then flip over, and it’s up to you to remember what was on them!
         Click on cards to match pairs of images until all cards have been matched in order to move on to the next level, where you will have twice as many cards to memorize and match!</p>
         <button type="button" onClick={() => {
-          console.log('test');
           setReturningUser(true);
-          hidePopup();
+          // hidePopup();
         }}>Close</button>
-        </span>
+        </div>
+      </div>
+      <div className="dropdown">
+        <form action="/playGame">
+          <select name="theme">
+            <option value="random">Select a Theme</option>
+            <option value="animals">Animals</option>
+            <option value="flowers">Flowers</option>
+            <option value="fruits">Fruits</option>
+          </select>
+          <br></br>
+        <input type="submit" value="Play Game!"></input>
+        </form>
       </div>
     </section>
   );
